1b083f366d88a9a71d570b9d28e22b5540cd04a0
Ganesh <gg450169@gmail.com>
2025-07-20 19:09:17 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+  function scrollToMenu() {
+    document.getElementById('exploreMenuSection').scrollIntoView({ behavior: 'smooth' });
+  }
+
+  function showVideo(){
+    window.open('https://youtu.be/mhDJNfV7hjk?si=1MKLDob-iX33-_yW','_blank');
+  }
